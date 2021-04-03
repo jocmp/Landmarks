2ba2f71d0c8bc8fc71fb13e34ac0f3bb4de36b48
Josiah Campbell <jocmp@hey.com>
2021-04-03 12:01:59 -0400

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Josiah Campbell on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
